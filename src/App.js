310d88411f0edc2023061a32a29ee61260896dca
Zafk1eI <sintfrank@gmail.com>
2024-02-13 19:28:28 +0300

    task 9

--- src/App.js
+++ src/App.js
@@ -3,16 +3,9 @@ import './App.css';
 function App() {
   return (
     <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>6</li>
-      <li>7</li>
-      <li>8</li>
-      <li>9</li>
-      <li>10</li>
+      <li>text1</li>
+      <li>text2</li>
+      <li>text3</li>
     </ul>
   );
 }
