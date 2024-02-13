7c1972ffc4c0dd1e7cb64ffe971b3c233cb2e2a1
Zafk1eI <sintfrank@gmail.com>
2024-02-13 19:26:52 +0300

    task 8

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p className='first'>Первый абзац</p>
-      <p className='second'>Второй абзац</p>
-    </div>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+    </ul>
   );
 }
 
