9cbe0cf501e22f3e558a64145d2050ecdaf5bfe3
Zafk1eI <sintfrank@gmail.com>
2024-02-21 08:38:42 +0300

    react 18-19

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 
 function App() {
-  const attr = 'block';
+  const li1 = <li>text1</li>;
+  const li2 = <li>text2</li>;
+  const li3 = <li>text3</li>;
 
   return (
-    <div id = {attr}>
-      text
-    </div>
+    <main>
+      {li1}
+      {li2}
+      {li3}
+    </main>
   );
 }
 
