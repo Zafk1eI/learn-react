d54d0fd2a634de93a4ac63d865582dd48d7034a6
Zafk1eI <sintfrank@gmail.com>
2024-02-21 08:28:25 +0300

    react 15

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 
 function App() {
-  const name = 'user';
-  const age = '30';
+  const arr = [1, 2, 3, 4, 5];
 
   return (
     <div>
-      name: {name}
-      age:  {age}
+      <ul>
+        <li>{arr[0]}</li>
+        <li>{arr[1]}</li>
+        <li>{arr[2]}</li>
+        <li>{arr[3]}</li>
+        <li>{arr[4]}</li>
+      </ul>
     </div>
   );
 }
