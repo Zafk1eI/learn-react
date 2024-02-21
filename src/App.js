6a71aca96f02900e885d58283d3c7d722f6c39aa
Zafk1eI <sintfrank@gmail.com>
2024-02-21 08:21:21 +0300

    react 13

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 
 function App() {
-	return (
-  <>
-  <input/><input/><input/>
-  </>
+	const str1 = 'text1';
+	const str2 = 'text1';
+
+  return (
+    <div>
+      <p>{str1}</p>
+      <p>{str2}</p>
+    </div>
   );
 }
 
