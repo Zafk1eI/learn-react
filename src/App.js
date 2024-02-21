272f05eedb0cc87ad2ca501315eb8892ff447790
Zafk1eI <sintfrank@gmail.com>
2024-02-21 08:43:41 +0300

    react_20

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 
 function App() {
-  const li1 = <li>text1</li>;
-  const li2 = <li>text2</li>;
-  const li3 = <li>text3</li>;
+  const items = (<>
+    <li>text1</li>
+    <li>text2</li>
+    <li>text3</li>
+    </>
+  );
 
   return (
-    <main>
-      {li1}
-      {li2}
-      {li3}
-    </main>
+    <ul>
+      {items}
+    </ul>
   );
 }
 
