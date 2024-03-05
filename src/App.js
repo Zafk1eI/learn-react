a20c62129a77a73946c513b8b04642c43ccfcbd2
Zafk1eI <sintfrank@gmail.com>
2024-03-05 17:22:36 +0300

    react 34

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
 
 function App() {
-  function show1() {
-    alert(1);
-  }
-
-  function show2() {
-    alert(2);
+  function showMess(text) {
+    alert(text);
   }
 
   return <div>
-    <button onClick={show1}>act1</button>
-    <button onClick={show2}>act2</button>
+    <button onClick={() => showMess('1')}>act1</button>
+    <button onClick={() => showMess('2')}>act2</button>
+    <button onClick={() => showMess('3')}>act3</button>
   </div>;
 }
 
