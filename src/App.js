c3e56b809c9a397600a5fa0cee3a8c2a57b855ce
Zafk1eI <sintfrank@gmail.com>
2024-03-05 17:53:32 +0300

    react 43

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import './App.css';
 
 function App() {
   const users = [
-    {id:1, name: 'user1', surn: 'surn1', age: 30 },
-    {id:2, name: 'user2', surn: 'surn2', age: 31 },
-    {id:3, name: 'user3', surn: 'surn3', age: 32 },
+    { id: 1, name: 'user1', surn: 'surn1', age: 30 },
+    { id: 2, name: 'user2', surn: 'surn2', age: 31 },
+    { id: 3, name: 'user3', surn: 'surn3', age: 32 },
   ];
-
-  const res = users.map(function (item, index) {
-    return <li key={index}>
-      <span>{item.name}</span>:
-      <span>{item.surn}</span>:
-      <span>{item.age}</span>
-    </li>;
+  const rows = users.map(function (item) {
+    return <tr key={item.id}>
+      <td>{item.name}</td>
+      <td>{item.surn}</td>
+      <td>{item.age}</td>
+    </tr>;
   });
 
-  return <ul>
-    {res}
-  </ul>;
+  return <table>
+    <thead>
+      <tr>
+        <td>Имя</td>
+        <td>Фамилия</td>
+        <td>Возраст</td>
+      </tr>
+    </thead>
+    <tbody>
+      {rows}
+    </tbody>
+  </table>;
 }
 
 export default App;
