fac396bb5aab4cbba009809c4d2fdf43164ae7d0
Zafk1eI <sintfrank@gmail.com>
2024-03-05 16:14:54 +0300

    react 31

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 
 function App() {
-  const isAuth = false;
-
-  return <div>
-    {!isAuth && <p>пожалуйста, авторизуйтесь</p>}
-  </div>;
+	function getdigitsum(num) {
+		num = String(num)
+    let result = 0
+    for (let i = 0; i < num.length; i++){
+      result += Number(num[i])
+    }
+    return result
+	}
+	
+	const sum = getdigitsum(123);
+	
+	return <div>
+		{sum}
+	</div>
 }
 
 export default App;
