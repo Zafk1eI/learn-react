508209c9bd4bd60d9b72255254a1f72f9bf6ec62
Zafk1eI <sintfrank@gmail.com>
2024-03-05 17:40:35 +0300

    react 39

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 
 function App() {
-  const arr = [];
-
-  for (let i = 1; i <= 5; i++) {
-    arr.push(<li>{i}</li>);
-  }
-
+  const arr = ['a', 'b', 'c', 'd', 'e'];
+  	
+	const res = arr.map(function(item) {
+		return <li>{item}</li>;
+	});
   return <ul>
-    {arr}
+    {res}
   </ul>;
 }
 
