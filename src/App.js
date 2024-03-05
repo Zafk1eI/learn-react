35c3453fa73d492ecff83eba2610c2fd64e54a62
Zafk1eI <sintfrank@gmail.com>
2024-03-05 17:31:33 +0300

    react 35

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 
 function App() {
-  function showMess(text) {
-    alert(text);
+  function func(event) {
+    console.log(event.target); // объект с событием
   }
 
   return <div>
-    <button onClick={() => showMess('1')}>act1</button>
-    <button onClick={() => showMess('2')}>act2</button>
-    <button onClick={() => showMess('3')}>act3</button>
+    <button onClick={func}>text</button>
   </div>;
 }
 
