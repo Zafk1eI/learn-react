7e91c40384b664916cd57695e907df55d372e5bc
Zafk1eI <sintfrank@gmail.com>
2024-03-05 15:53:26 +0300

    react 24

--- src/App.js
+++ src/App.js
@@ -1,46 +1,11 @@
 import './App.css';
 
 function App() {
-
-  return (
-    <>
-      <div>
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-          <li>4</li>
-          <li>5</li>
-          <li>6</li>
-          <li>7</li>
-          <li>8</li>
-          <li>9</li>
-          <li>10</li>
-        </ul>
-      </div>
-      <div>
-        <table style={{ border: '1px solid' }}>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
+	const num = 4;
+	
+	return <div>
+		result: {Math.sqrt(num)}
+	</div>;
 }
 
 export default App;
