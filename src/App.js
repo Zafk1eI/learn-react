9b1b122fd9eb64647c8a365bacd9136c3a9dd854
Zafk1eI <sintfrank@gmail.com>
2024-03-05 16:04:13 +0300

    react 29

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
 function App() {
-	const age = 19;
-	
-	return <div>
-		{age ? <p>больше 18</p> : <p>меньше 18</p>}
-	</div>;
+  const isAuth = true;
+
+  return <div>
+    {isAuth && <p>Вы авторизованы</p>}
+  </div>;
 }
 
 export default App;
