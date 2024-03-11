00992941b516a8482a42f378bc2b679bb2476c71
Zafk1eI <sintfrank@gmail.com>
2024-03-11 18:11:58 +0300

    react_48

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import './App.css';
 import uuid from 'react-uuid';
 
 function App() {
+  function id() {
+    return uuid()
+  }
   const array = ['one', 'two', 'three'];
   const res = array.map(function (item) {
-    return <p id={uuid()}>{item}</p>
+    return <p id={id()}>{item}</p>
   });
 
   return <div>
