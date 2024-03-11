8effe8e0ab56c6c29cfa59856df625ab55fb963f
Zafk1eI <sintfrank@gmail.com>
2024-03-11 18:14:52 +0300

    react_49

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ function App() {
   function id() {
     return uuid()
   }
-  const array = ['one', 'two', 'three'];
-  const res = array.map(function (item) {
-    return <p id={id()}>{item}</p>
-  });
+  const prods = [
+    { id: id(), name: 'product1', cost: 100 },
+    { id: id(), name: 'product2', cost: 200 },
+    { id: id(), name: 'product3', cost: 300 },
+  ];
+  const res = prods.map(function(item) {
+		return <p id={item.id}>
+			<span>{item.name}</span>:
+			<span>{item.cost}</span>
+		</p>;
+	});
 
   return <div>
     {res}
