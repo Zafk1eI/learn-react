eb54b70ffb4dad3e8e198715653b2167cbddae48
Zafk1eI <sintfrank@gmail.com>
2024-03-19 09:59:59 +0300

    react 57

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import './App.css';
 import React, { useState } from 'react';
 
 function App() {
-	const [count, setCount] = useState(0);
-	
-	return <div>
-		<span>{count}</span>
-		<button onClick={() => setCount(count + 1)}>+</button>
-    <button onClick={() => setCount(count - 1)}>-</button>
-	</div>;
+  const [value, setValue] = useState('');
+
+  return <div>
+    <input value={value} onChange={event => setValue(event.target.value)} />
+    <p>cout: {value.length}</p>
+  </div>;
 }
 
 export default App;
