8d2da04ee6b81f88776121962451ffe30d63235a
Zafk1eI <sintfrank@gmail.com>
2024-03-27 08:45:03 +0300

    react_66

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import './App.css';
 import React, { useState } from 'react';
 
 function App() {
-  const citys = ['Москва', 'Владивосток', 'Санкт-Петербург', 'Париж'];
   const [value, setValue] = useState('');
 
-  const options = citys.map((text, index) => {
-    return <option key={index}>{text}</option>;
-  });
-
   return <div>
-    <select value={value} onChange={(event) => setValue(event.target.value)}>
-      {options}
+    <select value={value} onChange={event => setValue(event.target.value)}>
+      <option value="1">от 0 до 12 лет</option>
+      <option value="2">от 13 до 17 лет</option>
+      <option value="3">от 18 до 25 лет</option>
+      <option value="4">старше 25 лет</option>
     </select>
     <p>
-      ваш выбор: {value}
+      {value === '1' && 'от 0 до 12 лет'}
+      {value === '2' && 'от 13 до 17 лет'}
+      {value === '3' && 'от 18 до 25 лет'}
+      {value === '4' && 'старше 25 лет'}
     </p>
   </div>;
 }
